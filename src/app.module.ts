48e6bfdff53686afb86e53d9e8c9c839d9c4e089
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-06-11 18:40:38 +0530

    Prisma Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
     }),
