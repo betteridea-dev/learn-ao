35586f1789cc2523c530d16033240b28631e5c00
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-06-12 18:19:38 +0530

    Body Validation with Pipes

--- src/course/course-enrollment/dto/enroll-course.dto.ts
+++ src/course/course-enrollment/dto/enroll-course.dto.ts
@@ -1,7 +1,10 @@
+import { IsNumber } from 'class-validator';
+
 export class EnrollCourseDTO {
   /**
    * Pass the id of desired course
    * @example: 1
    */
+  @IsNumber({}, { message: 'Course ID must be of type number' })
   courseId: number;
 }
--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -6,43 +6,46 @@ import {
   Param,
   Delete,
   UseGuards,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('course')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @Post()
   create() {
     return this.courseService.create();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.courseService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.courseService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.courseService.findOne(id);
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @Patch(':id')
-  update(@Param('id') id: string) {
+  update(@Param('id', ParseIntPipe) id: number) {
     console.log(`Course Updation - ${id}`);
 
     return this.courseService.update();
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
     console.log(`Course Deletion - ${id}`);
 
     return this.courseService.remove();
