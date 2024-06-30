aff07b3f74f2c83366d6d0367e6089edf4921f96
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-06-30 12:10:31 +0530

    Test

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { base64ToUint8Array } from 'src/lib/utils';
 import { User } from '@prisma/client';
-import crypto from 'crypto';
+// import crypto from 'crypto';
 
 class VerifyDTO {
   publicKey: string;
@@ -36,6 +36,8 @@ export class AuthService {
       'SHA-256',
       base64ToUint8Array(data),
     );
+
+    console.log('CRYPTO', crypto);
 
     // import public jwk for verification
     const verificationKey = await crypto.subtle.importKey(
